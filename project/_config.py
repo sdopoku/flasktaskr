e484c77f380c3af8c5e1101eb9c2fd53968a962c
David S Opoku <david@adoorelabs.com>
2015-09-22 14:51:32 +0000

    Create initial setup and login page.

---
+++ project/_config.py
@@ -0,0 +1,13 @@
+import os
+
+# grab the folder where this script lives
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DATABASE = 'flasktaskr.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'k+\xcd\xb9H\x85\x11\x82G\xa7\xb0\x93\x92Y[\xb8o\x01{\x0bs\x1fZ\x9b'
+
+# define the full path for the database
+DATABASE_PATH = os.path.join(basedir, DATABASE)
