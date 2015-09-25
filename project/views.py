0db3e6debd0cb14962da7dff1e397b2aab7c6c42
David S Opoku <david@adoorelabs.com>
2015-09-25 03:33:34 +0000

    Handle error when user tries to register with a username or password that already exists.

--- project/views.py
+++ project/views.py
@@ -9,6 +9,7 @@ from functools import wraps
 from flask import Flask, flash, redirect,\
 render_template, request, session, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
 
 # datetime
 import datetime
@@ -144,8 +145,12 @@ def register():
                 form.email.data,
                 form.password.data
             )
-            db.session.add(new_user)
-            db.session.commit()
-            flash('Thanks for registering. Please login.')
-            return redirect(url_for('login'))
+            try:
+                db.session.add(new_user)
+                db.session.commit()
+                flash('Thanks for registering. Please login.')
+                return redirect(url_for('login'))
+            except IntegrityError:
+                error = 'That username and/or email already exist.'
+                return render_template('register.html', form=form, error=error)
     return render_template('register.html', form=form, error=error)
